97899fd1c1c01c1546d4d2de1f841006316207d5
renzozuk <renzo.zuk19@gmail.com>
2024-06-18 02:04:15 -0300

    Word implemented

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,23 @@
+pub mod word;
+
+use word::build_word;
+
 fn main() {
+    let w1 = build_word(String::from("sabia"));
+    let w2 = build_word(String::from("sabiá"));
+    let w3 = build_word(String::from("sábia"));
+    let w4 = build_word(String::from("trem"));
+    let w5 = build_word(String::from("cinco"));
+    let w6 = build_word(String::from("língua"));
+    let w7 = build_word(String::from("TREM"));
+    
+    println!("{}", w1 == w2);
+    println!("{}", w2 == w3);
+    println!("{}", w1 == w3);
+    
+    println!("{}", w1 == w4);
+    println!("{}", w2 == w5);
+    println!("{}", w3 == w6);
+    
+    println!("{}", w4 == w7);
 }
---
+++ src/word.rs
@@ -0,0 +1,50 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Word {
+    word: String,
+}
+
+impl fmt::Display for Word {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.word)
+    }
+}
+
+impl PartialEq for Word {
+    fn eq(&self, other: &Word) -> bool {
+        if self.word.chars().count() != other.word.chars().count() {
+            return false;
+        }
+        
+        for (l1, l2) in self.word.to_lowercase().chars().zip(other.word.to_lowercase().chars()) {
+            if !letters_are_equal(l1, l2) {
+                return false;
+            }
+        }
+        
+        true
+    }
+}
+
+pub fn build_word(word: String) -> Word {
+    Word {
+        word,
+    }
+}
+
+fn letters_are_equal(first_letter: char, second_letter: char) -> bool {
+   normalize_char(first_letter) == normalize_char(second_letter)
+}
+
+fn normalize_char(c: char) -> char {
+    match c {
+        'á' | 'à' | 'â' | 'ã' => 'a',
+        'é' | 'è' | 'ê' => 'e',
+        'í' | 'ì' | 'î' => 'i',
+        'ó' | 'ò' | 'ô' | 'õ' => 'o',
+        'ú' | 'ù' | 'û' | 'ü' => 'u',
+        'ç' => 'c',
+        _ => c,
+    }
+}
